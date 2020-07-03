7b264196b04e2c69c7567e4ac6abdc0c75164c2d
Giuseppe Rotolo <rotolo37@gmail.com>
2020-07-03 14:25:39 +0200

    aggiornamento 2.0
    Cambiate funzioni -- eliminate alcune funzioni e aggiunte altre.

--- js/myscript.js
+++ js/myscript.js
@@ -1,99 +1,38 @@
 $(document).ready(function() {
-  // al click del tasto cerca
-  $(document).on('click', 'button', function(){
-   ricercaFilm();
-   ricercaSerieTv();
+  // al click del tasto cerca si apre l'input
+  $(document).on('click', 'i', function(){
+    $('input').show(250);
+    $('i').hide();
 
+  });
 
- });
   // quando premo invio parte la ricerca
   $('input').keyup(function() {
     if (event.which === 13 || event.keyCode === 13) {
-      ricercaFilm();
-      ricercaSerieTv();
-
+      var query = $('input').val();
+      cancellaContenuto();
+      ricerca('search/movie', query);
+      $('input').hide();
+      $('i').show(500);
     }
   });
 
-
-// ------------ FUNZIONI -------------------
-
-// Funzione per la ricerca dei film
-function ricercaFilm() {
-  var errore = $('.errore').remove();
-  var filmRicercato = $('input').val();
-  var movies = $('.movies').remove();
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie',
-    type: 'GET',
-    data: {
-      api_key : 'da6638a7fb98426de45fd1717f524c48',
-      query : filmRicercato,
-      language: 'it-IT'
-    },
-    success: function (data) {
-      var datiRisultati = data.results;
-      stampaFilm(datiRisultati);
-    },
-    error:(function() {
-      var messaggioErrore = 'inserisci qualcosa da cercare';
-      nessunRisultato(messaggioErrore);
-    })
+  // la passaggio del mouse sulle copertine
+  $(document).on('mouseenter', '.movies', function(){
+    $(this).find('.text-movies').addClass('active');
   });
-};
 
-// Funzione per stampare la ricerca dei film
-function stampaFilm(film) {
-  for (var i = 0; i < film.length; i++) {
-    var titoloFilm = film[i].title;
-    var titoloOriginaleFilm = film[i].original_title;
-    var linguaFilm = film[i].original_language;
-    var votoFilm = film[i].vote_average;
-    var posterFilm = film[i].poster_path;
-    var context = {
-      poster: 'https://image.tmdb.org/t/p/w300' + posterFilm,
-      genere: 'Film',
-        title: titoloFilm,
-        originalTitle: titoloOriginaleFilm,
-        lingua: linguaBandiera(linguaFilm),
-        voto: stellinevoti(votoFilm)
-      };
-    var source = $("#movies-template").html();
-    var template = Handlebars.compile(source);
-    var html = template(context);
-    $('main .container .d-flex').append(html);
-  };
+  // quando il mouse esce dalle copertine
+ $(document).on('mouseleave', '.movies', function(){
+   $(this).find('.text-movies').removeClass('active');
+  });
 
-  };
 
-// Funzione per mostrare un messaggio in caso di nessun risultato
-function nessunRisultato(messaggio, val1, val2) {
-  if (val1 === 0 && val2 === 0) {
-    var source = $("#error-template").html();
-    var template = Handlebars.compile(source);
-    var context = {
-      body: messaggio
-    };
-    var html = template(context);
-    $('main .container .d-flex').append(html);
-    console.log('sono qui');
-  }
+  // ------------ FUNZIONI -------------------
 
-};
-
-// funziona che nasconde il titolo originale se lo stesso e' uguale al titolo
-function confrontaTitolo(titoloOriginale, titolo) {
-  var appoggio = false;
-	if ( titoloOriginale === titolo ) {
-    return appoggio = true;
-  }
-  else {
-    return appoggio = false;
-  }
-};
-
-// funzione che trasforma il valore(Testo) della lingua in emoji (bandiere)
-function linguaBandiera(lingua){
+  // funzione che trasforma il valore(Testo) della lingua in emoji (bandiere)
+  function linguaBandiera(lingua){
+    var lingua = lingua.original_language;
     if(lingua == 'it'){
       return '🇮🇹';
     }
@@ -106,84 +45,97 @@ function linguaBandiera(lingua){
     if(lingua == 'en'){
       return '🇺🇸';
     }
-}
-
-// funzione che trasforma un numero decimale in un numero intero
-function trasformaNumero(num){
-  var num = num / 2;
-  // con math ceil arrotondo il numero in eccesso
-  var numeroarrotondato = Math.ceil(num);
-  return numeroarrotondato;
-};
-
-// funzione che converte il voto e stampa delle stelline in base al voto del film
-function stellinevoti(numero) {
-  var numero = numero / 2;
-  // con math ceil arrotondo il numero in eccesso
-  var numeroarrotondato = Math.ceil(numero);
-  if (numeroarrotondato == 1) {
-    return '★☆☆☆☆';
-  }
-  if (numeroarrotondato == 2) {
-    return '★★☆☆☆';}
-  if (numeroarrotondato == 3) {
-    return '★★★☆☆';
-  }
-  if (numeroarrotondato == 4) {
-    return '★★★★☆';
-  }
-  if (numeroarrotondato == 5) {
-    return '★★★★★';
   }
 
-};
+  // funzione che converte il voto e stampa delle stelline in base al voto del film
+  function stellinevoti(numero) {
+    var numero = numero.vote_average / 2;
+    // con math ceil arrotondo il numero in eccesso
+    var numeroarrotondato = Math.ceil(numero);
+    if (numeroarrotondato == 1) {
+      return '★☆☆☆☆';
+    }
+    if (numeroarrotondato == 2) {
+      return '★★☆☆☆';
+    }
+    if (numeroarrotondato == 3) {
+      return '★★★☆☆';
+    }
+    if (numeroarrotondato == 4) {
+      return '★★★★☆';
+    }
+    if (numeroarrotondato == 5) {
+      return '★★★★★';
+    }
+  };
 
-// funzione per la ricerca delle serie tv
-function ricercaSerieTv() {
-  var serietvRicercata = $('input').val();
-  $.ajax({
-    url: 'https://api.themoviedb.org/3/search/tv',
-    type: 'GET',
-    data: {
-      api_key : 'da6638a7fb98426de45fd1717f524c48',
-      query : serietvRicercata,
-      language: 'it-IT'
-    },
-    success: function (data) {
-      var datiRisultati = data.results;
-      stampaSerieTv(datiRisultati);
+  // funzione che seleziona la chiave corretta per il titolo/nome
+  function scegliTitolo(oggetto) {
+    var titolo;
+    if (!oggetto.title) {
+      titolo = oggetto.original_name;
+    } else {
+      titolo = oggetto.original_title;
+    }
+    return titolo;
+  }
 
-    },
-    error:(function() {
+  // funzione per ricercare nel database un film o una serie tv
+  function ricerca(linkFinale, query) {
+  // con la ricerca faccio partire la chiamata AJAX
+    $.ajax({
+      url: "https://api.themoviedb.org/3/" + linkFinale,
+      type: 'GET',
+      data: {
+        api_key: 'da6638a7fb98426de45fd1717f524c48',
+        query: query,
+      },
+      success: function (data) {
+        var datiRisultati = data.results;
+        stampaRicerca(datiRisultati);
+      },
+      error: function (richiesta, stato, errore) {
+        // qui devo stampare qualcosa
+      }
+  })
+    $('input').val('');
+  };
 
-    })
-  });
-};
+  // funzione che compila l'handlebar e stampa i risultati
+  function stampaRicerca(datiRisultati) {
 
-// funzione per stampare la ricerca delle serie tv
-function stampaSerieTv(serieTv) {
-  for (var i = 0; i < serieTv.length; i++) {
-    var titoloserieTv = serieTv[i].name;
-    var titoloOriginaleserieTv = serieTv[i].original_name;
-    var linguaserieTv = serieTv[i].original_language;
-    var votoserieTv = serieTv[i].vote_average;
-    var posterSerieTv = serieTv[i].poster_path;
-    var context = {
-        poster: 'https://image.tmdb.org/t/p/w300' + posterSerieTv,
-        genere: 'Serie Tv',
-        title: titoloserieTv,
-        originalTitle: titoloOriginaleserieTv,
-        lingua: linguaBandiera(linguaserieTv),
-        voto: stellinevoti(votoserieTv)
-      };
     var source = $("#movies-template").html();
     var template = Handlebars.compile(source);
-    var html = template(context);
-    $('main .container .d-flex').append(html);
+
+    // genero contenuto per l handlebars
+    datiRisultati.forEach(function(item) {
+
+      var context = {
+        poster: copertina(item),
+          title: scegliTitolo(item),
+          lingua: linguaBandiera(item),
+          voto: stellinevoti(item)
+        };
+
+      var html = template(context);
+      $(".content-movie").append(html);
+    });
   };
-}
 
+  // funzione che rimuove il contenuto
+  function cancellaContenuto() {
+    $(".content-movie").html('');
+  }
 
-
+  // funzone che stampa la copertina del film/serie tv
+  function copertina(oggetto) {
+    var immagineCopertina = '';
+    if (oggetto.poster_path) {
+      immagineCopertina = 'https://image.tmdb.org/t/p/w342/' + oggetto.poster_path;
+    } else {
+      immagineCopertina = 'img/noimg.jpg';
+    }
+    return immagineCopertina
+  }
 
 });
